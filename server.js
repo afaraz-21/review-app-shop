fd4836690a5044eb2f28943983ddb00147c58961
afaraz-21 <a.faraz@spctek.com>
2026-08-24 17:29:36 +0500

    Update server.js

--- server.js
+++ server.js
@@ -43,12 +43,11 @@ const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
 const SHEET_NAME = 'Sheet1';
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: './google-creds.json',
+  credentials: sheetsCredentials,
   scopes: [
     'https://www.googleapis.com/auth/spreadsheets'
   ],
 });
-
 const sheets = google.sheets({
   version: 'v4',
   auth
